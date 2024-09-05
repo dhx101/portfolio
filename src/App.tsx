b1f4fbd802c258f1ca389f9911ddf81014162e4b
David Huang Xie <138037766+dhx101@users.noreply.github.com>
2024-09-05 12:49:37 +0200

    Changed paths to browsepath instead of router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
-import './App.css'
-import { Outlet } from 'react-router-dom'
-import Contact from './components/Contact'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+// import { Outlet } from 'react-router-dom'
+import Contact from "./components/Contact";
+import Home from "./pages/Home";
+import Project from "./pages/Proyectos";
 
 function App() {
-
-
-  return (
-    <>
-      <Outlet/>
-      <Contact/>
-    </>
-  )
+	return (
+		<>
+			<BrowserRouter>
+				<Routes>
+					<Route
+						path="/portfolio"
+						element={<Home />}
+					/>
+          <Route
+						path="/portfolio/proyectos"
+						element={<Project />}
+					/>
+				</Routes>
+			</BrowserRouter>
+			{/* <Outlet/> */}
+			<Contact />
+		</>
+	);
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,28 +3,28 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./pages/Home.tsx";
-import Project from "./pages/Proyectos.tsx";
+// import { BrowserRouter, createBrowserRouter, Route, RouterProvider, Routes } from "react-router-dom";
+// import Home from "./pages/Home.tsx";
+// import Project from "./pages/Proyectos.tsx";
 
-const router = createBrowserRouter([
-	{
-		path: "/portfolio/",
-		element: <App />,
-		children: [
-			{
-				path: "/portfolio/",
-				element: <Home />,
-			},
-      {
-				path: "/portfolio/proyectos",
-				element: <Project />,
-			},
-		],
-	},
-]);
+// const router = createBrowserRouter([
+// 	{
+// 		path: "/portfolio/",
+// 		element: <App />,
+// 		children: [
+// 			{
+// 				path: "/portfolio/",
+// 				element: <Home />,
+// 			},
+//       {
+// 				path: "/portfolio/proyectos",
+// 				element: <Project />,
+// 			},
+// 		],
+// 	},
+// ]);
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<RouterProvider router={router}/>
+		<App/>
 	</StrictMode>
 );
